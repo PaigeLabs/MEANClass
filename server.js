bf952ea90fcc85d47149177530ae416a3e472f8f
Lee Brandt <leebrandt@gmail.com>
2014-06-10 22:53:36 -0500

    Added html render engine (EJS)

--- server.js
+++ server.js
@@ -3,11 +3,13 @@
 var express = require('express');
 
 var server = express();
+server.engine('html', require('ejs').renderFile);
+server.set('view engine', 'html');
 
 server.get('/', function(req,res){
-    res.send(200, 'You got it!');
+    res.render('index');
 });
 
 server.listen(3000, '0.0.0.0', function(){
     console.log('Express server listening on 0.0.0.0:3000...');
-})
+});
