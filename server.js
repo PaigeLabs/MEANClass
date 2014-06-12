6fe15270e5ef2e4fbe1c9f2c0ec4bbb89fd43400
Lee Brandt <leebrandt@gmail.com>
2014-06-12 12:49:06 -0500

    removed cookie parser & method override. Not sure they'll be needed with Express 4.0

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+    bodyParser = require('body-parser');
+
 
 var server = express();
 server.engine('html', require('ejs').renderFile);
 server.set('view engine', 'html');
+server.use(bodyParser());
+
 
 server.get('/', function(req,res){
-    res.render('index');
+   res.render('index');
 });
 
-server.listen(3000, '0.0.0.0', function(){
+var router = express.Router();
+
+server.use('/api', router);
+
+router.get('/', function(req,res){
+   res.json({'message':'we are aliiiive'});
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port, '0.0.0.0', function(){
     console.log('Express server listening on 0.0.0.0:3000...');
 });
